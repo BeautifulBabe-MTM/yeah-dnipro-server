b056869be2625f4707eb57462115cc479d309179
BeautifulBabe-MTM <86706552+BeautifulBabe-MTM@users.noreply.github.com>
2024-01-03 17:05:40 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect('mongodb+srv://admin:123zxc34@cluster0.hoxv5bc.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
 });
 
 const projectSchema = new mongoose.Schema({
